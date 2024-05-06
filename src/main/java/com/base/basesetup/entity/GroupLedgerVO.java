063600ba4f2233e133e2f5c83d596601e883c356
vasanth <vasanth@whydigit.com>
2024-05-06 17:40:19 +0530

    structure for groupledger,account screen

---
+++ src/main/java/com/base/basesetup/entity/GroupLedgerVO.java
@@ -0,0 +1,71 @@
+package com.base.basesetup.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.base.basesetup.dto.CreatedUpdatedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "hsnsaccode")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupLedgerVO {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "groupledgergen")
+	@SequenceGenerator(name = "hsnsaccodegen", sequenceName = "groupledgerseq", initialValue = 1000000001, allocationSize = 1)
+	@Column(name = "groupledgerid")
+	private Long gLId;
+	@Column(name = "groupname", length=50)
+	private String groupNmae;
+	@Column(name = "gsttaxflag")
+	private boolean gstTaxFlag;
+	@Column(name = "accountcode", length=20)
+	private String accountCode;
+	@Column(name = "coalist", length=30)
+	private String coaList;
+	@Column(name = "accountgroupname", length=30)
+	private String accountgroupName;
+	@Column(name = "type", length=30)
+	private String type;
+	@Column(name = "interbranchac", length=30)
+	private boolean interBranchAc;
+	@Column(name = "controllac", length=30)
+	private boolean controllAc;
+	@Column(name = "category", length=50)
+	private String category;
+	@Column(name = "currency", length=50)
+	private String currency;
+	@Column(name = "branch", length=50)
+	private String branch;
+	
+	
+	@Column(name = "createdby", length=30)
+	private String createdBy;
+	@Column(name = "updatedby", length=30)
+	private String updatedBy;
+	@Column(name = "cancelremarks", length=50)
+	private String cancelRemarks;
+	private boolean cancel;
+	private boolean active;
+	
+	
+	
+	
+	@Embedded
+	@Builder.Default
+	private CreatedUpdatedDate commonDate = new CreatedUpdatedDate();
+}
