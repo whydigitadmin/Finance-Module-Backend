4d9860d7733b4ee16729a256f5e48a0efb5244bc
vasanth <vasanth@whydigit.com>
2024-05-07 11:00:53 +0530

    cross orgin added

--- src/main/java/com/base/basesetup/controller/UserController.java
+++ src/main/java/com/base/basesetup/controller/UserController.java
@@ -19,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,6 +39,7 @@ import com.base.basesetup.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController extends BaseController {
 	public static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
