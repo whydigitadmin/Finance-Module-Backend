2a8d9426f74589bc7d282d397aa901f64c2ef00b
Guhanwhydigit <guhan@whydigit.in>
2024-05-23 20:17:42 +0530

    extra import removed

--- src/main/java/com/base/basesetup/controller/BasicMasterController.java
+++ src/main/java/com/base/basesetup/controller/BasicMasterController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -11,15 +12,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.base.basesetup.common.CommonConstant;
 import com.base.basesetup.common.UserConstants;
@@ -43,7 +47,10 @@ import com.base.basesetup.entity.FinancialYearVO;
 import com.base.basesetup.entity.ResponsibilitiesVO;
 import com.base.basesetup.entity.RoleMasterVO;
 import com.base.basesetup.entity.StateVO;
+import com.base.basesetup.exception.ApplicationException;
 import com.base.basesetup.service.BasicMasterService;
+
+import io.jsonwebtoken.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -267,6 +274,24 @@ public class BasicMasterController extends BaseController {
 		}
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
+	}
+
+	@PostMapping("/uploadCompanyLogo")
+	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file, @RequestParam("id") Long id)
+			throws IOException, java.io.IOException, ApplicationException {
+		CompanyVO image = basicMasterService.saveImage(file, id);
+		return ResponseEntity.status(HttpStatus.OK).body("Company Logo uploaded successfully: " + image.getId());
+	}
+
+	@GetMapping("/getCompanyLogo/{id}")
+	public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
+		Optional<CompanyVO> imageOptional = basicMasterService.getImage(id);
+		if (imageOptional.isPresent()) {
+			CompanyVO image = imageOptional.get();
+			return ResponseEntity.ok().header("attachment; filename=\"" + image.getImageName() + "\"")
+					.body(image.getData());
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 
 //	Employee -----------------------------------------------------------------------------------
--- src/main/java/com/base/basesetup/dto/RoleMasterDTO.java
+++ src/main/java/com/base/basesetup/dto/RoleMasterDTO.java
@@ -1,7 +1,5 @@
 package com.base.basesetup.dto;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
