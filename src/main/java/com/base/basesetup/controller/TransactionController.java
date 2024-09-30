fb205f2825713a6b3d2eb8711250f3b973c33b44
DhineshWDS <dhinesh@whydigit.in>
2024-09-30 17:31:12 +0530

    error fixing

--- src/main/java/com/base/basesetup/controller/TransactionController.java
+++ src/main/java/com/base/basesetup/controller/TransactionController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -2191,7 +2192,7 @@ public class TransactionController extends BaseController {
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 
-	   
+	}
 	//Reconcile
 	
 	@GetMapping("/getAllReconcileByOrgId")
