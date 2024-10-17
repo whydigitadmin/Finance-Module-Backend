edc92c3f238b0f9bdd559269b8ee4ebd1ff4b61d
narasimhawds <narashima@whydigit.in>
2024-10-17 18:20:29 +0530

    PaymentVoucher Error Fixing

--- src/main/java/com/base/basesetup/dto/PaymentVoucherDTO.java
+++ src/main/java/com/base/basesetup/dto/PaymentVoucherDTO.java
@@ -1,6 +1,7 @@
 package com.base.basesetup.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,12 +17,23 @@ public class PaymentVoucherDTO {
 	private String vehicleSubType;
 	private String referenceNo;
 	private String currency;
-	private String docid;
-	private LocalDateTime referenceDate;
+	private String docId;
+	private LocalDate referenceDate;
 	private BigDecimal exRate;
-	private LocalDateTime docDate;
+	private LocalDate docDate;
+	private String chequeNo;
+	private LocalDate chequeDate;
+	private String chequeBank;
 	private String remarks;
+	private Long orgId;
+	private String branch;
+	private String branchCode;
 	private String createdBy;
+	private boolean active;
+	private boolean cancel;
+	private String cancelRemarks;
+	private String ipNo;
+	private String latitude;
 	private BigDecimal totalDebitAmount;
 	private BigDecimal totalCreditAmount;
 
