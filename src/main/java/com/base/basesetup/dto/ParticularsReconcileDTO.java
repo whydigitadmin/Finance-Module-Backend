6f913b3475116e9d60289eec19ff37da6458838c
Aravinth <justinaravinth@whydigit.com>
2024-10-23 12:12:31 +0530

    reconcile date format removed

--- src/main/java/com/base/basesetup/dto/ParticularsReconcileDTO.java
+++ src/main/java/com/base/basesetup/dto/ParticularsReconcileDTO.java
@@ -3,8 +3,6 @@ package com.base.basesetup.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +13,8 @@ import lombok.NoArgsConstructor;
 public class ParticularsReconcileDTO {
 	private Long id;
 	private String voucherNo;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
 	private LocalDate voucherDate;
 	private String chequeNo;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
 	private LocalDate chequeDate;
 	private BigDecimal deposit;
 	private BigDecimal withdrawal;
